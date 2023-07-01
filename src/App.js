ed170588db5f05f40a7d9a118fab6c05a0a4d7a8
Ali <ali.hounain@gmail.com>
2023-06-30 23:36:20 -0400

    Added some bs
    it doesnt work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-
+import  React, { useState } from'react';
 import './App.css';
 import Business from './Business';
 
 function App() {
+  const [business, setBusiness] = useState(
+    [ {
+      img: "https://images.pexels.com/photos/3660425/pexels-photo-3660425.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      name: "Burger King",
+      address: "123 googoo street",
+      city: "Wahoo!", 
+      state: " Googusville", 
+      zipcode: "02992", 
+      category: "Pizza", 
+      rating: "4.9", 
+      reviewCount: " 100000",
+    }
+    ]
+  );
   return (
     <div className="App">
-      <Business business = { Business } />
+      {business.map((business) => {
+        <Business 
+        img = {business.img}
+        name = {business.name}
+        address = {business.address}
+        city = {business.city}
+        state = {business.state}
+        zipcode = {business.zipcode}
+        category = {business.category}
+        rating = {business.rating}
+        reviewCount = {business.reviewCount}
+        />;
+      })}
     </div>
   );
 }
--- src/Business.js
+++ src/Business.js
@@ -1,9 +1,15 @@
 import React from 'react'; 
 
 const Business = ({ business}) => {
-    const { image, name = "Pizza", address = "123 googoo street", city = "Wahoo!", state = " Googusville", 
-        zipcode = "02992", category = "Pizza", rating = "4.9", 
-        reviewCount = " 100000" }  = business;
+    const { image = "https://images.pexels.com/photos/3660425/pexels-photo-3660425.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          , name = "Pizza", 
+            address = "123 googoo street",
+            city = "Wahoo!", 
+            state = " Googusville", 
+            zipcode = "02992", 
+            category = "Pizza", 
+            rating = "4.9", 
+            reviewCount = " 100000" }  = business;
 
 
 return (
